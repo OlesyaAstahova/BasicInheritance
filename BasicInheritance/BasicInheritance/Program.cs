b1109502961e6b9d744e700666825d1106acb14a
Олесуа и Юля <astahova.98@gmail.com>
2018-02-05 11:54:09 +0700

    Класс Car, моделирующий некоторые базовые детали автомобиля

---
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Inheritance *****\n");
+            //Создать экземпляр Car и установить максимальную скорость
+            Car myCar = new Car(80);
+
+            //Установить текущую скорость и вывести ее на консоль
+            myCar.Speed = 50;
+            Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+            Console.ReadLine();
+        }
+    }
+}
